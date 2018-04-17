c1ba8b2b4e347b574d61917e7c3ada5c7b9df98a
Andy Osborne <cao89@email.arizona.edu>
2018-04-17 09:49:19 -0700

    Remove hhvm and create failure

--- tests/EmailTest.php
+++ tests/EmailTest.php
@@ -26,7 +26,7 @@ final class EmailTest extends TestCase
     public function testCanBeUsedAsString(): void
     {
         $this->assertEquals(
-            'user@example.com',
+            'user@example.comm',
             Email::fromString('user@example.com')
         );
     }
